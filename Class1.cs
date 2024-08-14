e2e074ef0f8b312e06568c33902b1910d462da02
pjs70776 <patrick.j.stover.1985@gmail.com>
2024-08-14 08:25:59 -0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment4
+{
+    class Class1
+    {
+        //method1 addition
+        public static int method1(int x)
+        {
+            int Add = x + 5; return Add;
+        }
+        //method2 that takes a decimal and subtracts it
+        public static decimal method1(decimal y)
+        {
+            decimal Dec = y - 5;
+            int diff = Convert.ToInt32(Dec); return diff;
+        }
+        //method 3 that takes a string, converts it to an integer, and adds it
+        public static int method1(string z)
+        {
+            string stringToInt = z + 5;
+            int product = Convert.ToInt32(stringToInt); return product;
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //method 1
+            Console.WriteLine("What number do you want to select to do the math operation?");
+            Console.WriteLine("Enter a number to be added to 5");
+            int additon = Convert.ToInt32(Console.ReadLine());
+            int sum = Class1.method1(additon);
+            Console.WriteLine(sum + " is the answer.");
+
+            //method 2 with decimal (float)
+            Console.WriteLine("enter a decimal up to 6 decimal digits to be subtracted by 5");
+            decimal subtract = Convert.ToDecimal(Console.ReadLine());
+            decimal diff = Class1.method1(subtract);
+            Console.WriteLine(diff + " is the answer.");
+
+            //method 3 that takes a string, converts it to an integer
+            Console.WriteLine("Enter a string (Unicode), that will be converted to an integer, to be added by 5");
+            string mult = Convert.ToString(Console.ReadLine());  //converting string to intiger
+            int prod = Class1.method1(mult);
+            Console.WriteLine(prod + " is the answer.");
+            Console.ReadLine();
+        }
+    }
+    
+
+}
+
+
